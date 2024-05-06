81c19ddbdf00527900e782b3824f8bf609e2e4b6
TagidickTagidick <tagidick.tagidick@gmail.com>
2024-05-06 14:40:26 +0300

    add widgets

---
+++ smiraddWidget/smiraddWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  smiraddWidgetBundle.swift
+//  smiraddWidget
+//
+//  Created by Минь Дык Фам on 06.05.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct smiraddWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        smiraddWidget()
+        smiraddWidgetLiveActivity()
+    }
+}
