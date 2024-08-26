cd3be29589853cbf4015a31ba62c33a74425221b
TagidickTagidick <tagidick.tagidick@gmail.com>
2024-08-26 17:44:34 +0300

    add code alert

---
+++ smiradd/Settings/NotificationManager.swift
@@ -0,0 +1,8 @@
+//
+//  NotificationManager.swift
+//  smiradd
+//
+//  Created by Минь Дык Фам on 06.05.2024.
+//
+
+import Foundation
---
+++ smiradd/Widgets/PinEntryView.swift
@@ -0,0 +1,8 @@
+//
+//  PinEntryView.swift
+//  smiradd
+//
+//  Created by Минь Дык Фам on 09.07.2024.
+//
+
+import Foundation
